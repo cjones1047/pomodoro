b956e7908a4b70065c2e2285e79ecb8f87dbb9dd
cjones1047 <cjmj1047@gmail.com>
2022-12-08 06:28:00 -0600

    Inserted 'Timer' title into canvas

--- main.py
+++ main.py
@@ -25,6 +25,10 @@ canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_img = PhotoImage(file="tomato.png")
 canvas.create_image(100, 112, image=tomato_img)
 canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
-canvas.pack()
+canvas.grid(column=1, row=1)
+
+title = Label(text="Timer", font=(FONT_NAME, 40, "normal"))
+title.config(pady=10, fg=GREEN, bg=YELLOW)
+title.grid(column=1, row=0)
 
 window.mainloop()
